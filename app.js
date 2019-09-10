e6aa37b1dc90dcec1b8dbcaff1ebb736cd426198
MaikPython <soeoskarpaul@gmail.com>
2019-09-10 11:41:24 +0300

    heroku works with proccess.env.PORT

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = 3000
 const path = require("path")
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, "static", "index.html" ))
 })
 
-app.use(express.static("static"))
+app.use(express.static('static'))
 
 app.listen(process.env.PORT || PORT, () => {
     console.log("Server started", PORT);
