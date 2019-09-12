3b7a7ed795949a8b088dd40ae5125b2358ada3ee
MaikPython <soeoskarpaul@gmail.com>
2019-09-12 11:00:07 +0300

    Changes

--- static/main.js
+++ static/main.js
@@ -1,13 +1,38 @@
-const showItemProps = () =>{
-    console.log(window.location.search)
+
+
+const showItem = () =>{
     const urlParams = new URLSearchParams(window.location.search)
-    console.log(urlParams.get("title"));
+
     const title = urlParams.get("title")
     const cost = urlParams.get("cost")
     const pic = urlParams.get("src")
-    alert(`Title: ${title} cost: ${cost} pic: ${pic}`);
+
+    const container = document.createElement("div")
+    container.className = "item_container";
+
+    const itemImage = document.createElement("img")
+    itemImage.src = pic
+    itemImage.className = "item__image";
+    itemImage.width = 128
+    itemImage.height = 320
+
+    const itemTitle = document.createElement('p')
+    itemTitle.textContent = title
+    itemTitle.className = "item_title"
+
+    const itemCost = document.createElement('p')
+    itemCost.textContent = cost
+    itemCost.className = "item_cost"
+
+    container.append(itemImage)
+    container.append(itemTitle)
+    container.append(itemCost)
+    
+    document.body.appendChild(container)
+
+    console.log(container)
     
 }
 
-
-window.onload = showItemProps  
+    
+window.onload = showItem
