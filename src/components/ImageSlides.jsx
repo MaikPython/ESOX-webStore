6b0457ec18a54e01d6493ed36f668fc127aec05f
MaikPython <soeoskarpaul@gmail.com>
2019-09-30 13:34:56 +0300

    landing page done

--- src/components/ImageSlides.jsx
+++ src/components/ImageSlides.jsx
@@ -1,13 +1,22 @@
 import React from "react"
-
+import {Link} from 'react-router-dom';
 class ImageSlides extends React.Component{
     render(){
         return(
-                <div className="responsive">
-                    <p>Contents</p>
+                <div className="slider">
+                    <figure>
+                        <img src="/assets/background1.jpg"/>
+                        <img src="/assets/background2.jpg"/>
+                        <img src="/assets/background1.jpg"/>
+                    </figure>
+                    <Link to="/items">
+                        <button className="landing-btn">Mine poodi</button>
+                    </Link>
                 </div>
                         
         )
     }
 }
+
+
 export default ImageSlides
