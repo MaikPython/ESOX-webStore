4443d6911b16ac86e9effc20c0ed3b8d95922229
MaikPython <soeoskarpaul@gmail.com>
2019-10-04 11:41:25 +0300

    Icons added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom";
-
+import { profileIcon, cartIcon } from './../icons.js'
+import './header.css'
 
 const Header = () => {
     const logoPic = "/assets/logo.jpg"
@@ -10,8 +11,8 @@ const Header = () => {
                 <img src={logoPic} alt="logo" width="100" height="100"/>
             </Link>
             <div className="header-items">
-                <button className="shopping-cart">Karu</button>
-                <button className="login">Logi sisse/Loo kasutaja</button>
+                <img className="profile-icon"src={profileIcon} width="50" height="50"/>
+                <img className="cart-icon" src={cartIcon} width="50" height="50"/>
             </div>
         </div>
     )
